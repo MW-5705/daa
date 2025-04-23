53d67f1e432a9fc2ef92671fc65e91cce568104c
Mehar Walia <113458487+MW-5705@users.noreply.github.com>
2025-04-23 15:22:24 +0530

    Create kosaraju_algo.cpp

---
+++ graphs/kosaraju_algo.cpp
@@ -0,0 +1,32 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> visited()
+
+void dfs(vector <vector<int>> graph)
+{
+    void 
+}
+
+void kosaraju_algo(vector <vector <int>> graph)
+{
+    
+}
+
+int main() {
+    // Write C++ code here
+    
+    vector <vector <int>> graph = {
+        {0, 0, 0, 0, 0, 0}, 
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0},
+        {0, 1, 0, 0, 0, 0},
+        {1, 1, 0, 0, 0, 0},
+        {1, 0, 1, 0, 0, 0},
+    };
+    kahns_algo(graph);
+
+    return 0;
+}
